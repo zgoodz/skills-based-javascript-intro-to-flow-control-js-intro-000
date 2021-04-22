796d50dae2cfe4266a4ec02b5667f2803c02b7e9
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-22 17:43:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,6 +34,6 @@ function ternaryTeenager(age) {
 function switchAge(age) {
  switch (age) {
    case age >= 13 && age <= 19:
-     return ("You are a teenager")
+     return ("You are a teenager");
  }
 }
